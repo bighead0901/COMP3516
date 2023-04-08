6c4eda3a9c3230e28a038642f8837d4c1c0b90a2
a.moment.in.the.life.of <hotkivan@gmail.com>
2023-04-08 11:07:10 +0800

    init

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, View, ImageBackground } from 'react-native';
+import { BottomNavigation, Text } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const RecordRoute = () => {
+        return(
+            <ImageBackground source={require('./src/bg.png')} resizeMode="cover" style={styles.image}>
+                <Text style={styles.text}>Speech Liveness Detection</Text>
+            </ImageBackground>
+        )
+    }
+
+    const FilesRoute = () => {
+        return(
+            <Text>History</Text>
+        )
+    }
+
+    const [index, setIndex] = React.useState(0);
+    const [routes] = React.useState([
+    { key: 'record', title: 'Recording', focusedIcon: 'record-circle', unfocusedIcon: 'record-circle-outline'},
+    { key: 'files', title: 'History', focusedIcon: 'file', unfocusedIcon: 'file-outline' },
+    ]);
+
+    const renderScene = BottomNavigation.SceneMap({
+        record: RecordRoute,
+        files: FilesRoute,
+    });
+
+    return (
+        <SafeAreaProvider>
+            <BottomNavigation
+                navigationState={{ index, routes }}
+                onIndexChange={setIndex}
+                renderScene={renderScene}
+            />
+        </SafeAreaProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        width: "500",
+        height: "100%",
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    image: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    text: {
+        color: '#777777',
+        fontSize: 23,
+        lineHeight: 84,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
 });
